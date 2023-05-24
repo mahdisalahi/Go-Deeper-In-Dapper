591b3df4f672c1e34ac4b20e74c286e7f70104ff
Mr.Salahinia <salahimahdi938@gmail.com>
2023-05-25 02:07:33 +0430

    AddSqlConstantsAndSingleExecuteMethod

---
+++ TrainDapper/DapperExecute/ExecuteSample.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using TrainDapper.Constant;
+using TrainDapper.Helpers;
+
+namespace TrainDapper.DapperExecute
+{
+    public class ExecuteSample
+    {
+        public int CallCreateCourseStoreProcedureSingle()
+        {
+            using (var connection = DapperHelper.GetDbConnection())
+            {
+                var affectedRows = connection.Execute(SqlCommandCons.CallCreateCourseSP, new
+                {
+                    Title = "Math",
+                    TeacherName = "Teacher1",
+                    Capacity = 10
+                }, commandType: System.Data.CommandType.StoredProcedure);
+
+                return affectedRows;
+            }
+        }
+
+        public void CallCreateCourseStoreProcedureMultiple()
+        {
+            using (var connection = DapperHelper.GetDbConnection())
+            {
+                var affectedRows = connection.Execute(SqlCommandCons.CallCreateCourseSP, new
+                {
+                    Title = "Math",
+                    TeacherName = "Teacher1",
+                    Capacity = 10
+                }, commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+    }
+}
