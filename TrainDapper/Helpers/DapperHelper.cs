0b71f681abc5458b142ee0e75dd25d93494f6adf
Mr.Salahinia <salahimahdi938@gmail.com>
2023-05-24 04:58:47 +0430

    ImplementHelpersMethods

---
+++ TrainDapper/Helpers/DapperHelper.cs
@@ -0,0 +1,33 @@
+﻿using MiniProfiler.Integrations;
+using System.Data;
+using System.Data.SqlClient;
+using TrainDapper.Constant;
+
+namespace TrainDapper.Helpers
+{
+    public class DapperHelper
+    {
+        public static IDbConnection GetDbConnection()
+        {
+            return new SqlConnection(DbConstant.ConnectionString);
+        }
+
+        public static CustomDbProfiler GetCustomDbProfiler()
+        {
+            return CustomDbProfiler.Current;
+        }
+
+        public static IDbConnection ProfilerDbConnection()
+        {
+            var fac = new SqlServerDbConnectionFactory(DbConstant.ConnectionString);
+            return ProfiledDbConnectionFactory.New(fac, GetCustomDbProfiler());
+        }
+
+        public static string GetExecuteCommands()
+        {
+            var res = GetCustomDbProfiler().ProfilerContext.GetExecutedCommands();
+            GetCustomDbProfiler().ProfilerContext.Reset();
+            return res;
+        }
+    }
+}
