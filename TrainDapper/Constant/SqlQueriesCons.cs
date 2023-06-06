fb9a902e9982afeb1315297225fdafecc124ad1d
Mr.Salahinia <salahimahdi938@gmail.com>
2023-06-06 14:04:27 +0330

    AddQueryMethods

--- TrainDapper/Constant/SqlQueriesCons.cs
+++ TrainDapper/Constant/SqlQueriesCons.cs
@@ -5,16 +5,16 @@
         #region Students TB
 
         public static string GetStudentWithFirstName = "select * from Students where FirstName in @FirstNames";
-        private static string GetStudents = "select * from Students";
-        private static string GetCustomColumnsStudents = "select FirstName Name, LastName Family from Students";
-        private static string GetStudentsJoinWithStudentAdditionalInfo = "select * from Students as s inner join StudentAdditionalInfo as a  on s.id=a.id";
-        private static string GetStudentsJoinWithScores = "select * from Students as s inner join Scores as a  on s.id=a.studentId";
+        public static string GetStudents = "select * from Students";
+        public static string GetCustomColumnsStudents = "select FirstName Name, LastName Family from Students";
+        public static string GetStudentsJoinWithStudentAdditionalInfo = "select * from Students as s inner join StudentAdditionalInfo as a  on s.id=a.id";
+        public static string GetStudentsJoinWithScores = "select * from Students as s inner join Scores as a  on s.id=a.studentId";
 
         #endregion
 
         #region Scores TB
 
-        private static string GetScores = "select * from Scores";
+        public static string GetScores = "select * from Scores";
 
         #endregion
     }
---
+++ TrainDapper/DapperQuery/QuerySample.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using TrainDapper.Constant;
+using TrainDapper.Helpers;
+
+namespace TrainDapper.DapperQuery
+{
+    public class QuerySample
+    {
+        public void QueryAnonymous()
+        {
+            using (var connection = DapperHelper.ProfilerDbConnection())
+            {
+                var result = connection.Query(SqlQueriesCons.GetStudentWithFirstName, new { FirstNames = new[] { "TypeName", "TypeName" } });
+
+                foreach (var item in result)
+                {
+                    Console.WriteLine($"{item.FirstName}, {item.LastName}");
+                }
+
+                Console.WriteLine(DapperHelper.GetExecuteCommands());
+            }
+        }
+
+        public void QueryStronglyType()
+        {
+            using (var connection = DapperHelper.ProfilerDbConnection())
+            {
+                var result = connection.Query(SqlQueriesCons.GetStudents);
+
+                foreach (var item in result)
+                {
+                    Console.WriteLine($"{item.FirstName}, {item.LastName}");
+                }
+
+                Console.WriteLine(DapperHelper.GetExecuteCommands());
+            }
+        }
+    }
+}
